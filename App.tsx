089acfd46648633cff8f1e64ca07ecfbf6abaf23
Benedict Forrest <benedict.forrest@me.com>
2020-08-30 09:45:37 +1000

    add basic list of top-headlines

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+    // Articles state
+    const [articles, setArticles] = useState([])
+
+    // Fetch the articles from NewsAPI
+    const fetchMovies = () => {
+        fetch(`http://newsapi.org/v2/top-headlines?country=au&apiKey=96e7efbae84544aca2e40f5834bf2777`)
+          .then((response) => response.json())
+          .then((json) => setArticles(json.articles))
+          .catch((error) => console.log(error))
+    }
+
+    // run fetchMovies on inital render
+    useEffect(() => {
+        fetchMovies()
+    }, [])
+
     return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-        </View>
-    );
+        <FlatList
+            data={articles}
+            keyExtractor={(item, index: number) => index.toString()}
+            renderItem={({ item, index }) => (
+                <View>
+                    <Text>{item.title}</Text>
+                    <Text>{item.author}</Text>
+                    <Text>{item.description}</Text>
+                </View>
+            )}
+        />
+    )
 }
 
 const styles = StyleSheet.create({
